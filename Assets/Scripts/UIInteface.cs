3340b046ffa724ff7b59b40c693610f2937afa05
Anton Nedvedz <2595092@gmail.com>
2025-06-30 17:59:36 +0200

    Add mobile control and some refactor

--- Assets/Scripts/UIInteface.cs
+++ Assets/Scripts/UIInteface.cs
@@ -12,7 +12,7 @@ public class UIInteface : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -25,31 +25,35 @@ public class UIInteface : MonoBehaviour
             focusObj.GetComponent<Collider>().enabled = false;
 
         }
-        else if(Input.GetMouseButton(0))
+        else if(Input.GetMouseButton(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved))
         {
 
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(!Physics.Raycast(ray, out hit) || focusObj == null)
+            if(!Physics.Raycast(ray, out hit) || !focusObj)
             {
                 return;
             }
 
             focusObj.transform.position = hit.point + new Vector3(0, 1, 0);
         }
-        else if(Input.GetMouseButtonUp(0))
+        else if(focusObj && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out hit) &&
-                hit.collider.gameObject.CompareTag("Platform"))
+                hit.collider.gameObject.CompareTag("Platform") &&
+                hit.normal.Equals(new Vector3(0,1,0)))
             {
                 hit.collider.gameObject.tag = "Occupied";
+                focusObj.transform.position = new Vector3(hit.collider.gameObject.transform.position.x, focusObj.transform.position.y, hit.collider.gameObject.transform.position.z);
             }
             else
             {
                 Destroy(focusObj);
             }
+
+            focusObj = null;
 
         }
     }
